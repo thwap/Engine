377f2586b8bbcc3cca14397960a46e71b7bbc847
Susikettu <zx3end@gmail.com>
2013-09-17 10:52:13 +0300

    Added EngineObject,GameObject,Scene,Component initial

---
+++ Engine/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class GameObject : EngineObject
+    {
+        private bool _updated;
+        private List<Component> _components;
+        public virtual void initGameObject()
+        {
+        }
+        public void Render()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public bool isUpdated()
+        {
+            return _updated;
+        }
+    }
+}
---
+++ Engine/Scene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class Scene : EngineObject
+    {
+        public virtual void initScene()
+        {  
+
+        }
+        public virtual void Run()
+        {
+
+        }
+        public virtual void Render()
+        {
+
+        }
+    }
+}
