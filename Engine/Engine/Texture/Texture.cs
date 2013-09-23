07a8a91666cd3cb5c1baf91b58de6cf7e175c46c
Janne <janne.pellosniemi@metropolia.fi>
2013-09-23 13:35:32 +0300

    Texture class

---
+++ Engine/Engine/Texture/Texture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Engine.Texture
+{
+    class Texture
+    {
+        int OpenGLID;
+        int textureID;
+        int width;
+        int height;
+
+        public Texture(int OpenGLID, int textureID, int width, int height) {
+            this.OpenGLID = OpenGLID;
+            this.textureID = textureID;
+            this.width = width ;
+            this.height = height;
+        }
+
+
+
+
+
+
+    }
+}
