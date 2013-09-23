13ceb81ab5cb8123db8f1a7a58f9c3a814bc0738
Ghettoburger <senssi@gmail.com>
2013-09-23 14:19:58 +0300

    Texture add to project

---
+++ Engine/Engine/Texture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Engine.Texture {
+   
+    public struct Texture {
+        int OpenGLID;
+        int textureID;
+        int width;
+        int height;
+        
+    
+        public Texture(int OpenGLID, int textureID, int width, int height):this() {
+            this.OpenGLID = OpenGLID;
+            this.textureID = textureID;
+            this.width = width ;
+            this.height = height;
+        }
+    }
+}
