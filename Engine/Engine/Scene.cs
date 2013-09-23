9f0247c8a6f12caaea7240715dab3c2316450343
thwap <sami.kuisma@metropolia.fi>
2013-09-23 14:07:45 +0300

    scene..

--- Engine/Engine/Scene.cs
+++ Engine/Engine/Scene.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Engine.Engine
 {
-    class Scene:EngineObject
+    class Scene:EngineObject,IDisposable
     {
+        protected List<GameObject> GameObjects;
+
+        public Scene(String name)
+        {
+            App.scenes.add(name, this);
+        }
+
+        public virtual void Update()
+        {
+            foreach(GameObject GO in GameObjects){
+              //  GO.Update();
+            }
+        }
+        public virtual void Render()
+        {
+            // do nothing.
+        }
+        
+        
+
+   }
+
+    class Scene1 : Scene
+
+    {
+        public Scene1()
+            : base("Scene1")
+        {
+        }
+
+        public Scene1(String name)
+            : base(name)
+        {
+        }
+    }
+
+    class Scene2 : Scene
+    {
+
     }
 }
