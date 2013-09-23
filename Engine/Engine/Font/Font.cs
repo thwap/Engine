eee90f1ff3da93f727dbf8d4b6def8ceef071ebb
Janne <janne.pellosniemi@metropolia.fi>
2013-09-23 14:33:37 +0300

    font testing....INCOMPLETE.....

---
+++ Engine/Engine/Font/Font.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Engine.Engine.Font
+{
+    class Font {
+        
+        Font font;
+        //int fontSize;
+
+
+        public Font(String fontName, float fontSize){
+            try{
+                this.font = new Font(fontName, fontSize);
+            }catch (Exception e){
+                Console.Write("Font not found" + e);
+                this.font = new Font("Times New Roman", 12.0f);
+            }
+        }
+    }
+}
