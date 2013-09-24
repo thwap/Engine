c822f28eed230f06a46bdbc594f2ec8211d234cc
unknown <SYSTEM@Ciedu.metropolia.fi>
2013-09-24 11:04:40 +0300

    Automagic documentation update

--- search/classes_6d.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['mathf',['Mathf',['../class_engine_1_1_mathf.html',1,'Engine']]],
-  ['matrix',['Matrix',['../class_engine_1_1_matrix.html',1,'Engine']]],
-  ['message',['Message',['../struct_engine_1_1_message.html',1,'Engine']]]
-];
--- search/files_6c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['loop_2ecs',['Loop.cs',['../_loop_8cs.html',1,'']]]
-];
