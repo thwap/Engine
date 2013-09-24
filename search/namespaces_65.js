2d7ffc9c102e154bb39fb39f3c7f531a9ebfdd25
unknown <SYSTEM@Ciedu.metropolia.fi>
2013-09-24 10:35:07 +0300

    Automagic documentation update

--- search/namespaces_65.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['engine',['Engine',['../namespace_engine.html',1,'']]],
-  ['engine',['Engine',['../namespace_engine_1_1_engine.html',1,'Engine']]],
-  ['properties',['Properties',['../namespace_engine_1_1_properties.html',1,'Engine']]]
-];
--- search/variables_61.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['a',['a',['../struct_engine_1_1_colorf.html#a574680743799f00d7ed274003060bdb3',1,'Engine::Colorf']]]
-];
