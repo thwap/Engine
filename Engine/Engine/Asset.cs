4d0486c07f6d3e03e222794357c2f92a0e72a7c8
Mikko Lindell <mikkolindell@hotmail.fi>
2013-09-24 11:34:03 +0300

    Add initial Asset

---
+++ Engine/Engine/Asset.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using Tao.DevIl;
+
+
+namespace Engine {
+
+    public class Asset {
+        
+    }
+
+    public class AudioAsset : Asset {
+        //Tao.OpenAl ...
+        AudioAsset() {
+
+        }
+    }
+    public class TextureAsset : Asset {
+        //Tao.DevIl ...
+        TextureAsset() {
+
+        }
+    }
+}
