24e65514d7f8370e2106ecf23f8623806a6b133b
anatolil <anatoli.lipiainen@metropolia.fi>
2013-09-24 10:31:04 +0300

    Some updates to Transform.cs

--- Engine/Engine/Transform.cs
+++ Engine/Engine/Transform.cs
@@ -23,14 +23,25 @@ namespace Engine
    
         //Move
         public void Translate(Vector3 tr)
-        { 
-            
+        {
+            // position.x = position.x + tr.x;
+            // position.y = position.y + tr.y;
+            // position.z = position.z + tr.z;
+
+            Matrix m = new Matrix();
+
+            m.SetTranslation(tr.x, tr.y, tr.z);
+            position = m.GetTranslation();
         }
 
         //Rotate
         public void Rotate(float angle)
-        { 
-            
+        {
+            Matrix m = new Matrix();
+            m.SetRotateZ(angle);
+            // rotation = rotation * m      // would be a good idea to change rotation vector to vector4
+                                            // so it would be possible to do multiplication
+
         }
 
         //Scale
