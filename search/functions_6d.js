ce4d2a70fc82ce34e368a4fabfb590bf5982953f
unknown <SYSTEM@Ciedu.metropolia.fi>
2013-09-24 11:04:53 +0300

    Automagic documentation update

---
+++ search/functions_6d.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['matrix',['Matrix',['../class_engine_1_1_matrix.html#a05de165c84237ca5a0fd308682e88499',1,'Engine.Matrix.Matrix()'],['../class_engine_1_1_matrix.html#aadbfad414618d6f64d8f49c0673dfc6c',1,'Engine.Matrix.Matrix(Matrix m)'],['../class_engine_1_1_matrix.html#a43b4b8e7b96934c0cf05d4e79547de40',1,'Engine.Matrix.Matrix(Vector4 x, Vector4 y, Vector4 z, Vector4 w)']]],
+  ['max',['Max',['../class_engine_1_1_mathf.html#a3e3276777efdfe37942afe962adfc15c',1,'Engine.Mathf.Max(float a, float b)'],['../class_engine_1_1_mathf.html#aa6b2349f58b98063a925c4703c6570fb',1,'Engine.Mathf.Max(params float[] values)']]],
+  ['min',['Min',['../class_engine_1_1_mathf.html#aa21694215aefe70efefa2ed0437de990',1,'Engine.Mathf.Min(float a, float b)'],['../class_engine_1_1_mathf.html#a2001dcb51a7dab5855ca5f5ff6258574',1,'Engine.Mathf.Min(params float[] values)']]],
+  ['movestep',['MoveStep',['../struct_engine_1_1_vector3.html#a4eb7094ebb6f36cae7f4cdf68fbd2705',1,'Engine.Vector3.MoveStep()'],['../struct_engine_1_1_vector2.html#a6eeee8fff1846231e862f998a4b3f72f',1,'Engine.Vector2.MoveStep()'],['../struct_engine_1_1_vector4.html#a9581d0989caf7079e5a683da764d81f7',1,'Engine.Vector4.MoveStep()']]],
+  ['movetowards',['MoveTowards',['../class_engine_1_1_mathf.html#af5369a426eabf1156e5ab2dba1a82684',1,'Engine::Mathf']]],
+  ['mulmatrix',['MulMatrix',['../class_engine_1_1_matrix.html#a7b2f0592f882a589c32fd7cee4e0f832',1,'Engine::Matrix']]]
+];
---
+++ search/variables_65.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['epsilon',['Epsilon',['../class_engine_1_1_mathf.html#a50ee816fadf897fc1b20c365447e648a',1,'Engine::Mathf']]]
+];
