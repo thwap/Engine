26569f45382cba9a3e298731fc3964f5a1cf0d67
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2013-09-24 10:50:42 +0300

    Skeleton of content-class

--- Engine/Engine/Content.cs
+++ Engine/Engine/Content.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Engine.Engine
+
+namespace Engine
 {
-    class Content
+    public static class Content
     {
+        static Dictionary<string, object> assetList = new Dictionary<string,object>();
+        
+        public static void init(string directory = "Content")
+        {
+            if (Directory.Exists(directory))
+            {
+                string[] files = Directory.GetFiles(directory);
+                foreach (string file in files)
+                {
+                    //TODO
+                }
+            }
+            else
+            {
+                string error = "The contend directory  " + directory + " does not exist";
+                System.Diagnostics.Debug.Assert(false, error);
+            }
+        }
+        public static T Load<T>(string asset) {
+            throw new NotImplementedException();
+        }
+
+        static Texture LoadTexture() {
+            throw new NotImplementedException();
+        }
     }
 }
