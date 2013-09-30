9d3e403b311fc26c1949555893edfd982de4a76b
Janne Pellosniemi <pellos2011@gmail.com>
2013-09-30 15:18:44 +0300

    sprite class fixes

--- Engine/Engine/Sprite.cs
+++ Engine/Engine/Sprite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +11,41 @@ using Tao.Platform.Windows;
 
 namespace Engine {
     public class Sprite : Component {
-        Transform transform;
+        Transform transform { get;set; }
         Vector3[] vertex;
-        Vector2[] vertexUVs;
-        Texture texture;
-        Colorf[] vertexColor;
-        int vertexAmount;
+        Vector2[] vertexUVs {get;set;}
+        Texture texture {get;set;}
+        Color[] vertexColor {get;set;}
+        int vertexAmount {get;set;}
 
+        Sprite() {
+            
+        }
 
-        public void InitVertexPositions(Vector3 position, float width, float height) {
-        
+        public void InitVertexPositions(Vector3 position, float width, float height)
+        {
+            vertex = new Vector3[6];
+            vertex[3] = vertex[0] =
+                new Vector3(width / 2, height / 2, 0) + position; // upperright
+            vertex[1] =
+                new Vector3(width / 2, -height / 2, 0) + position; // lowerright
+            vertex[4] = vertex[2] =
+                new Vector3(-width / 2, -height / 2, 0) + position; // lowerleft
+            vertex[5] =
+                new Vector3(-width / 2, +height / 2, 0);  //topleft
+
         }
 
         public void Draw() {
-        
+            foreach (Vector3 vertx in vertex) {
+
+            }
         }
 
-        public void DrawVertex(Vector3 position, Colorf color, Vector2 uvs) {
-        
+        public void DrawVertex(Vector3 position, Color color, Vector2 uvs) {
+            Gl.glColor4f(color.R, color.G, color.B, color.A);
+            Gl.glTexCoord2f(uvs.x, uvs.y);
+            Gl.glVertex3f(position.x, position.y, position.z);
         }
         public void ApplyMatrix(Matrix compositeMatrix) {
             foreach (Vector3 v in vertex) {
@@ -36,7 +54,8 @@ namespace Engine {
         }
 
         public void SetPosition(Vector3 position) {
-       
+               
+                   
         }
     }
 }
