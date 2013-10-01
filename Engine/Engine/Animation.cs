f88e6b612529bad79e87b6508c8055e0a337a428
mikaal <Mika.Alasalmi@metropolia.fi>
2013-10-01 10:02:21 +0300

    Animation, AnimationClip, texture public changes

---
+++ Engine/Engine/Animation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class Animation
+    {
+        public bool playing = true;
+        AnimationClip currentAnimation;
+        internal static Dictionary<string, AnimationClip> animationList;
+
+        public void Play(string name)
+        {
+            currentAnimation = animationList[name];
+            if (Time.time > currentAnimation.prevDeltatime + currentAnimation.speed)
+            {
+                currentAnimation.prevDeltatime = Time.time;
+
+                currentAnimation.sprite.texture = Il.ilBlit(currentAnimation.texture, 0, 0, 0, currentAnimation.yCoordinate, currentAnimation.xCoordinate, 0, currentAnimation.widthOne, currentAnimation.heightOne, 1);
+                currentAnimation.xCoordinate += currentAnimation.widthOne;
+                if (currentAnimation.xCoordinate == currentAnimation.texture.width)
+                {
+                    currentAnimation.yCoordinate += currentAnimation.heightOne;
+                    currentAnimation.xCoordinate = 0;
+                }
+                if (currentAnimation.yCoordinate == currentAnimation.texture.height)
+                {
+                    if (currentAnimation.loop == true)
+                    {
+                        currentAnimation.yCoordinate = currentAnimation.yStart;
+                        currentAnimation.xCoordinate = currentAnimation.xStart;
+                    }
+                }
+            }
+        }
+        public void Stop()
+        {
+            playing = false;
+        }
+    }
+}
